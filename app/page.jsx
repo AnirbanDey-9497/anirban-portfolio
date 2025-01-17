43c6fa8d6d8a1de0526c768ccc6353cee8088458
AnirbanDey-9497 <62869656+AnirbanDey-9497@users.noreply.github.com>
2025-01-17 01:41:54 -0500

    Added my socials to the website

--- app/page.jsx
+++ app/page.jsx
@@ -1,56 +1,46 @@
-import Image from 'next/image';
+import {Button} from "@/components/ui/button";
+import {FiDownload} from "react-icons/fi";
+import {Section} from "lucide-react";
 
-export default function Home() {
-  return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main>homepage</main>
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-          <a
-              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            <Image
-                aria-hidden
-                src="/file.svg"
-                alt="File icon"
-                width={16}
-                height={16}
-            />
-            Learn
-          </a>
-          <a
-              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            <Image
-                aria-hidden
-                src="/window.svg"
-                alt="Window icon"
-                width={16}
-                height={16}
-            />
-            Examples
-          </a>
-          <a
-              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-              href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            <Image
-                aria-hidden
-                src="/globe.svg"
-                alt="Globe icon"
-                width={16}
-                height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
-      </div>
+//components
+import Social from "@/components/Social";
+
+const Home = () => {
+  return(
+      <section className="h-full">
+        <div className="container mx-auto h-full">
+          <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+            {/*text*/}
+            <div className="text-center xl:text-left">
+              <span className="text-xl">Full Stack Software Developer</span>
+              <h1 className="h1 mb-6">
+                Hey this is <br /><span className="text-accent">Anirban Dey</span>
+              </h1>
+              <p className="max-w-[500px] mb-9 text-white/80">
+                  Job summary
+              </p>
+                {/*Social Media*/}
+                <div className="flex flex-col xl:flex-row items-center gap-8">
+                    <Button
+                     variant="outline"
+                     size="lg"
+                     className="uppercase flex items-center gap-2">
+                        <span>Download Resume</span>
+                        <FiDownload className="text-xl"/>
+                    </Button>
+                    <div className="mb-8 xl:mb-0">
+                        <Social
+                            containerStyles="flex gap-6"
+                            iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div>photo</div>
+          </div>
+        </div>
+      </section>
   );
-}
+};
+
+export default Home;
