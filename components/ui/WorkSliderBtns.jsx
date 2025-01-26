c06be375229c38cd1f17a7144599a4685694cd99
AnirbanDey-9497 <62869656+AnirbanDey-9497@users.noreply.github.com>
2025-01-25 23:54:04 -0500

    Adding buttons on project

---
+++ components/ui/WorkSliderBtns.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import {useSwiper} from "swiper/react";
+import {PiCaretLeftBold, PiCaretRightBold} from "react-icons/pi";
+
+const WorkSliderBtns = ({containerStyles, btnStyles, iconStyles}) => {
+    return(
+        <div>WorkSliderBtns</div>
+    );
+};
+
+{/*testing Git*/}
+export default WorkSliderBtns;
