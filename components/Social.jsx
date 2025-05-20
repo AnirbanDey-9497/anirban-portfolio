346dd164684907a90e39254c455619d953a89e9f
ANIRBAN DEY <62869656+AnirbanDey-9497@users.noreply.github.com>
2025-05-19 22:40:44 -0400

    Added social media profiles to the Home Page

--- components/Social.jsx
+++ components/Social.jsx
@@ -4,17 +4,33 @@ import {FaGithub, FaLinkedinIn} from "react-icons/fa"
 
 
 const socials = [
-    {icon: <FaGithub/>, path:""},
-    {icon: <FaLinkedinIn/>, path: ""}
+    {
+        icon: <FaGithub/>,
+        path: "https://github.com/AnirbanDey-9497",
+        label: "GitHub Profile"
+    },
+    {
+        icon: <FaLinkedinIn/>,
+        path: "https://linkedin.com/in/anirbandey9497",
+        label: "LinkedIn Profile"
+    }
 ]
+
 const Social = ({containerStyles, iconStyles }) => {
     return (
     <div className={containerStyles}>
         {socials.map((item, index) => {
         return(
-        <Link key={index} href={item.path} className={iconStyles}>
-        {item.icon}
-    </Link>
+        <Link 
+            key={index} 
+            href={item.path} 
+            className={iconStyles}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={item.label}
+        >
+            {item.icon}
+        </Link>
         );
         })}
     </div>
